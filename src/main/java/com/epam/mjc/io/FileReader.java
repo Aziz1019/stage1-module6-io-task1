167b2ca2b176d7d44ac38e018e15071476b2a417
Aziz1019 <starinc1998@gmail.com>
2023-01-27 12:54:54 +0500

    Minor Change

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -27,36 +27,4 @@ public class FileReader {
 
         return profile;
     }
-
-    public static void main(String[] args) {
-        FileReader fileReader = new FileReader();
-        Profile dataFromFile = fileReader.getDataFromFile(new File("D:\\epam-prep\\epam-lab\\mjc\\stages\\stage1-module6-io-task1\\src\\main\\resources\\Profile.txt"));
-        System.out.println(dataFromFile.getName() + " " + dataFromFile.getAge() + " " + dataFromFile.getEmail() + " " + dataFromFile.getPhone());
-
-    }
 }
-
-/*
-       // File path is passed as parameter
-        File file = new File(
-            "C:\\Users\\pankaj\\Desktop\\test.txt");
-
-        // Note:  Double backquote is to avoid compiler
-        // interpret words
-        // like \test as \t (ie. as a escape sequence)
-
-        // Creating an object of BufferedReader class
-        BufferedReader br
-            = new BufferedReader(new FileReader(file));
-
-        // Declaring a string variable
-        String st;
-        // Condition holds true till
-        // there is character in a string
-        while ((st = br.readLine()) != null)
-
-            // Print the string
-            System.out.println(st);
-    }
-}
-* */
